1669f81d4db9224c11e9f24998d662378aa7a2df
masudaso <masuda.so.23@gmail.com>
2024-10-06 16:19:42 +0900

    コミット

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Diary
+from .models import CustomUser
 
 # Register your models here.
-admin.site.register(Diary)
+admin.site.register(CustomUser)
---
+++ accounts/tests.py
@@ -0,0 +1,37 @@
+from django.test import LiveServerTestCase
+from django.urls import reverse_lazy
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class TestLogin(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        # ログインページを開く
+        self.selenium.get('http://localhost:8000' + str(reverse_lazy('account_login')))
+
+        # メールアドレス欄の入力
+        username_input = self.selenium.find_element(By.NAME, "login")
+        username_input.send_keys('masuda.so.23@gmail.com')
+        # パスワード欄の入力
+        password_input = self.selenium.find_element(By.NAME, "password")
+        password_input.send_keys('Shiki923')
+        # ログインボタンのクリック
+        button = self.selenium.find_element(By.XPATH, '//*[@id="wrapper"]/div/div/div/form/button')
+        button.click()
+
+        # 指定秒数待機(ページ遷移のため)
+        time.sleep(3)
+
+        # ページタイトルの検証
+        self.assertEqual('日記一覧 | Private Diary', self.selenium.title)
